27519ff2550427c3172716a7b171cbf948dd4a99
Vlad2346543 <114310954+Vlad2346543@users.noreply.github.com>
2026-03-02 02:08:35 +0200

    add serverApi

---
+++ lib/api/serverApi.ts
@@ -0,0 +1,54 @@
+import { cookies } from 'next/headers';
+import { api } from './api';
+import { User } from '@/types/user';
+
+
+export const fetchNotes = async () => {
+  const cookieStore = cookies();
+
+  const res = await api.get('/notes', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return res.data;
+};
+
+
+export const fetchNoteById = async (id: string) => {
+  const cookieStore = cookies();
+
+  const res = await api.get(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return res.data;
+};
+
+export const getMe = async () => {
+  const cookieStore = cookies();
+
+  const res = await api.get<User>('/users/me', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return res.data;
+};
+
+
+export const checkSession = async () => {
+  const cookieStore = cookies();
+
+  const res = await api.get<User | null>('/auth/session', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return res.data;
+};
